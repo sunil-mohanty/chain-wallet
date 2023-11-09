d5f14cd12852921c287abc9455325a873d7eaa92
sunil.mohanty@live.com <sunil.mohanty@live.com>
2023-11-09 06:52:41 +0400

    Updated to add the hamburger menu

--- lib/chain-work/Products.dart
+++ lib/chain-work/Products.dart
@@ -11,6 +11,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late List<Block> _blocks = [];
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   void initState() {
     super.initState();
@@ -25,34 +27,93 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      key: scaffoldKey,
+      /*appBar: AppBar(
         title: const Text('Transations'),
+      ),*/
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: _blocks.length,
+            itemBuilder: (context, index) {
+              return CardContentWidget(_blocks, index);
+            },
+          ),
+          Positioned(
+            top: 16.0,
+            left: 16.0,
+            child: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                // Open the drawer using the GlobalKey
+                scaffoldKey.currentState?.openDrawer();
+              },
+            ),
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: _blocks.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.all(8.0),
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _blocks[index].timestamp,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 8.0),
-                  Text(_blocks[index].data),
-                ],
+      drawer: Drawer(
+        // Drawer content goes here
+        child: DrawerContent(),
+      ),
+    );
+  }
+}
+
+class CardContentWidget extends StatelessWidget {
+  List<Block> _blocks;
+  int index;
+
+  CardContentWidget(this._blocks, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              _blocks[index].timestamp,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          );
-        },
+            SizedBox(height: 8.0),
+            Text(_blocks[index].data),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class DrawerContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        // Drawer content goes here
+        ListTile(
+          leading: Icon(Icons.home),
+          title: Text('Home'),
+          onTap: () {
+            // Handle drawer item tap
+            Navigator.pop(context); // Close the drawer
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Settings'),
+          onTap: () {
+            // Handle drawer item tap
+            Navigator.pop(context); // Close the drawer
+          },
+        ),
+      ],
     );
   }
 }
--- lib/chain-work/constants.dart
+++ lib/chain-work/constants.dart
@@ -2,6 +2,6 @@ class ApiConstants {
   //static String baseUrl = 'https://jsonplaceholder.typicode.com';
   static String baseUrl = 'http://localhost:3001';
   //static String usersEndpoint = '/users';
-  static String blocks = '/blocks';
-  static String login = '/login';
+  static String blocks = '/api/blocks/list';
+  static String login = '/api/login';
 }
